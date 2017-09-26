862f1ce4f88421a4ff8b529546ebec691955be70
iitpranata <iitpranataip@gmail.com>
2017-09-26 22:21:20 +0700

    tambah dikit-dikit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = "https://".$_SERVER['HTTPS_HOST'];
+$root = "http://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url']    = "$root";
 
--- application/controllers/Depan.php
+++ application/controllers/Depan.php
@@ -5,7 +5,7 @@ class Depan extends CI_Controller {
 
 	public function __construct ()
 	{
-    	parent:: __construct();
+		parent:: __construct();
    		$this->load->model('Depan_model');
 
   	}
